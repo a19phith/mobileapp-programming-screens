04b971f83f58937d74e9dc9d2e33ee23e4ccb5d6
a19phith <philip2leo@gmail.com>
2020-05-08 19:27:24 +0200

    deleted the second activity and replaced with fragment blank. all the settings are the same

--- app/src/main/java/com/example/screens/detaljer.java
+++ app/src/main/java/com/example/screens/detaljer.java
@@ -9,6 +9,6 @@ public class detaljer extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detaljer);
+        setContentView(R.layout.fragment_blank);
     }
 }
